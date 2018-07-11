5f3f0d5123c8aa1bcab279f25fc1882f4a73bc3b
hcascardo <humberto.demolinari@gmail.com>
2018-07-11 11:23:37 -0300

    createcol test 4 - nullnullnull + msg

--- server.js
+++ server.js
@@ -123,9 +123,9 @@ app.get('/createcol', function (req, res) {
         var data = db.collection('data');
         // Create a document with data -- This data will be updated by a POST req from ESP32
         data.insert({ humidity: 20, temperature: 25, airhum: 17 });
-        res.render('createcol.html', { humidity: null, temperature: null, airhum: null });
+        res.render('createcol.html', { humiditymsg: null, temperature: null, airhum: null });
     } else {
-        res.render('createcol.html', { humidity: null, temperature: null, airhum: null });
+        res.render('createcol.html', { humiditymsg: null, temperature: null, airhum: null });
     }
 });
 
